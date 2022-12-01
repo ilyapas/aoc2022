5e430d0c10bba489a57cfd739ee3b9ea43db02fe
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-01 20:28:36 +0100

    Solve Day01-Puzzle1

--- src/day01.rs
+++ src/day01.rs
@@ -1,5 +1,20 @@
 pub mod puzzle1 {
     pub fn solve() {
-        println!("Hello, world!");
+        let input = include_str!("../input/day01_puzzle1.txt");
+        let mut sum = 0;
+        let mut max_sum: i32 = 0;
+        for line in input.lines() {
+            if line == "" {
+                sum = 0;
+                continue;
+            }
+            let value = line.parse::<i32>().unwrap();
+            sum += value;
+            if sum > max_sum {
+                max_sum = sum;
+                println!("{} {}", sum, max_sum);
+            }
+        }
+        println!("Day 01 - Puzzle 1: {}", max_sum);
     }
 }
