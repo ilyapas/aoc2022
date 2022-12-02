649881868bea2f4dec94b25ebb6bfb5b39fd4e55
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-02 21:07:00 +0100

    Solve Day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,20 +1,35 @@
 use std::collections::HashMap;
 
 pub fn solve() {
-    let scoring_map = HashMap::from([
+    let scoring_map_one = HashMap::from([
         ('A', HashMap::from([('X', 4), ('Y', 8), ('Z', 3)])),
         ('B', HashMap::from([('X', 1), ('Y', 5), ('Z', 9)])),
         ('C', HashMap::from([('X', 7), ('Y', 2), ('Z', 6)])),
     ]);
 
+    let scoring_map_two = HashMap::from([
+        ('A', HashMap::from([('X', 3), ('Y', 4), ('Z', 8)])),
+        ('B', HashMap::from([('X', 1), ('Y', 5), ('Z', 9)])),
+        ('C', HashMap::from([('X', 2), ('Y', 6), ('Z', 7)])),
+    ]);
+
     let input = std::fs::read_to_string("input/day02.prod.txt").unwrap();
-    let result: usize = input
+    let result = input
         .lines()
         .map(|line| {
             let theirs = line.chars().nth(0).unwrap();
             let ours = line.chars().nth(2).unwrap();
-            scoring_map.get(&theirs).unwrap().get(&ours).unwrap()
+            [
+                scoring_map_one.get(&theirs).unwrap().get(&ours).unwrap(),
+                scoring_map_two.get(&theirs).unwrap().get(&ours).unwrap(),
+            ]
         })
-        .sum();
-    println!("Day 02 - Part One: {}", result);
+        .fold([0, 0], |mut acc, score| {
+            acc[0] += score[0];
+            acc[1] += score[1];
+            acc
+        });
+
+    println!("Day 02 - Part One: {}", result[0]);
+    println!("Day 02 - Part Two: {}", result[1]);
 }
