a6934bb40e906f51e96b07b7fdff13a24c50da46
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-04 08:08:14 +0100

    Solve Day 4

--- src/day04.rs
+++ src/day04.rs
@@ -1,6 +1,6 @@
 pub fn solve() {
     let input = std::fs::read_to_string("input/day04.prod.txt").unwrap();
-    let result = input
+    let (result_one, result_two) = input
         .lines()
         .map(|line| {
             line.split_terminator(',').map(|range| {
@@ -13,10 +13,22 @@ pub fn solve() {
         .map(|ranges| {
             let (start_one, end_one) = ranges.clone().nth(0).unwrap();
             let (start_two, end_two) = ranges.clone().nth(1).unwrap();
-            (start_one >= start_two && end_one <= end_two)
-                || (start_two >= start_one && end_two <= end_one)
+            let part_one = (start_one >= start_two && end_one <= end_two)
+                || (start_two >= start_one && end_two <= end_one);
+            let part_two = part_one
+                || (start_one >= start_two && start_one <= end_two)
+                || (end_one >= start_two && end_one <= end_two);
+            (part_one, part_two)
         })
-        .filter(|&x| x)
-        .count();
-    println!("Day 04 - Part One: {}", result);
+        .fold((0, 0), |mut acc, result| {
+            if result.0 {
+                acc.0 += 1;
+            }
+            if result.1 {
+                acc.1 += 1;
+            }
+            acc
+        });
+    println!("Day 04 - Part One: {}", result_one);
+    println!("Day 04 - Part Two: {}", result_two);
 }
