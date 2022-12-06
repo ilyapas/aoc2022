757b251a5cb037dbd8afa8eba3fb73944cf01945
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-06 08:49:49 +0100

    Solve Day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,20 +1,25 @@
 use std::collections::HashSet;
 use std::collections::VecDeque;
 
-pub fn solve() {
-    let input = std::fs::read_to_string("input/day06.prod.txt").unwrap();
+fn find_marker(tag: &str, input: &str, marker_size: usize) {
     let mut queue: VecDeque<char> = VecDeque::new();
     for (i, c) in input.chars().enumerate() {
-        if queue.len() < 4 {
+        if queue.len() < marker_size {
             queue.push_back(c);
         } else {
             queue.pop_front();
             queue.push_back(c);
             let set = queue.iter().collect::<HashSet<&char>>();
-            if set.len() == 4 {
-                println!("Day 06 - Part One: {}", i + 1);
+            if set.len() == marker_size {
+                println!("Day 06 - Part {}: {}", tag, i + 1);
                 break;
             }
         }
     }
+}
+
+pub fn solve() {
+    let input = std::fs::read_to_string("input/day06.prod.txt").unwrap();
+    find_marker("One", &input, 4);
+    find_marker("Two", &input, 14);
 }
