7531d67be78f365d98d5921df9065e481309a03e
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-08 22:06:24 +0100

    Solve Day 8

--- src/day08.rs
+++ src/day08.rs
@@ -1,44 +1,76 @@
 use std::cmp::max;
 
-fn largest_above(digits: &Vec<isize>, index: usize, width: usize) -> isize {
-    let mut result: isize = -1;
+fn check_above(digits: &Vec<isize>, index: usize, width: usize) -> (isize, isize) {
+    let mut tallest: isize = -1;
+    let mut view: isize = 0;
+    let mut max_view_found = false;
     let row = index / width;
     for i in 1..=row {
         let above = index - i * width;
-        result = max(result, digits[above]);
+        tallest = max(tallest, digits[above]);
+        if !max_view_found {
+            view += 1;
+            if digits[above] >= digits[index] {
+                max_view_found = true;
+            }
+        }
     }
-    result
+    (tallest, view)
 }
 
-fn largest_below(digits: &Vec<isize>, index: usize, width: usize) -> isize {
-    let mut result: isize = -1;
+fn check_below(digits: &Vec<isize>, index: usize, width: usize) -> (isize, isize) {
+    let mut tallest: isize = -1;
+    let mut view: isize = 0;
+    let mut max_view_found = false;
     let row = index / width;
     let height = digits.len() / width;
     for i in 1..(height - row) {
         let below = index + i * width;
-        result = max(result, digits[below]);
+        tallest = max(tallest, digits[below]);
+        if !max_view_found {
+            view += 1;
+            if digits[below] >= digits[index] {
+                max_view_found = true;
+            }
+        }
     }
-    result
+    (tallest, view)
 }
 
-fn largest_left(digits: &Vec<isize>, index: usize, width: usize) -> isize {
-    let mut result: isize = -1;
+fn check_left(digits: &Vec<isize>, index: usize, width: usize) -> (isize, isize) {
+    let mut tallest: isize = -1;
+    let mut view: isize = 0;
+    let mut max_view_found = false;
     let col = index % width;
     for i in 1..=col {
         let left = index - i;
-        result = max(result, digits[left]);
+        tallest = max(tallest, digits[left]);
+        if !max_view_found {
+            view += 1;
+            if digits[left] >= digits[index] {
+                max_view_found = true;
+            }
+        }
     }
-    result
+    (tallest, view)
 }
 
-fn largest_right(digits: &Vec<isize>, index: usize, width: usize) -> isize {
-    let mut result: isize = -1;
+fn check_right(digits: &Vec<isize>, index: usize, width: usize) -> (isize, isize) {
+    let mut tallest: isize = -1;
+    let mut view: isize = 0;
+    let mut max_view_found = false;
     let col = index % width;
     for i in 1..(width - col) {
         let right = index + i;
-        result = max(result, digits[right]);
+        tallest = max(tallest, digits[right]);
+        if !max_view_found {
+            view += 1;
+            if digits[right] >= digits[index] {
+                max_view_found = true;
+            }
+        }
     }
-    result
+    (tallest, view)
 }
 
 pub fn solve() {
@@ -51,14 +83,20 @@ pub fn solve() {
         .collect::<Vec<isize>>();
 
     let mut visible: isize = 0;
+    let mut max_scenic_score: isize = 0;
     for (i, d) in digits.iter().enumerate() {
-        let above = largest_above(&digits, i, width);
-        let below = largest_below(&digits, i, width);
-        let left = largest_left(&digits, i, width);
-        let right = largest_right(&digits, i, width);
-        if d > &above || d > &below || d > &left || d > &right {
+        let (tallest_above, view_above) = check_above(&digits, i, width);
+        let (tallest_below, view_below) = check_below(&digits, i, width);
+        let (tallest_left, view_left) = check_left(&digits, i, width);
+        let (tallest_right, view_right) = check_right(&digits, i, width);
+        if d > &tallest_above || d > &tallest_below || d > &tallest_left || d > &tallest_right {
             visible += 1;
         }
+        max_scenic_score = max(
+            max_scenic_score,
+            view_above * view_below * view_left * view_right,
+        );
     }
     println!("Day 08 - Part One: {}", visible);
+    println!("Day 08 - Part Two: {}", max_scenic_score);
 }
