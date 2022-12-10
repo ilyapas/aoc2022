4ce912d132321f5327425bb2f8a14ed874b58dfe
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-10 09:23:50 +0100

    Solve Day 10

--- src/day10.rs
+++ src/day10.rs
@@ -2,6 +2,7 @@ pub fn solve() {
     let input = std::fs::read_to_string("input/day10.prod.txt").unwrap();
     let mut x: Vec<isize> = vec![1];
     let mut signal: isize = 0;
+    let mut pixels: Vec<char> = vec![];
     for line in input.lines() {
         match line.split_whitespace().collect::<Vec<&str>>().as_slice() {
             ["noop"] => x.push(*x.last().unwrap()),
@@ -13,11 +14,22 @@ pub fn solve() {
             _ => (),
         }
     }
-
     [20, 60, 100, 140, 180, 220].iter().for_each(|&i: &usize| {
         signal += i as isize * x[i - 1];
     });
-
+    x.iter().enumerate().for_each(|(i, sprite)| {
+        if [*sprite - 1, *sprite, *sprite + 1].contains(&(i as isize % 40)) {
+            pixels.push('#');
+        } else {
+            pixels.push('.');
+        }
+    });
     println!("Day 10 - Part One: {}", signal);
-    // println!("{:#?}", x);
+    println!("Day 10 - Part Two:");
+    for i in 0..(pixels.len() / 40) {
+        println!(
+            "{}",
+            pixels[i * 40..(i + 1) * 40].iter().collect::<String>()
+        );
+    }
 }
