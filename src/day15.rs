2881d248908ace1e1344bea4c554ed0de1730202
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-15 09:56:41 +0100

    Solve Day 15

--- src/day15.rs
+++ src/day15.rs
@@ -29,23 +29,56 @@ pub fn solve() {
                 c.get(3).unwrap().as_str().parse::<isize>().unwrap(),
                 c.get(4).unwrap().as_str().parse::<isize>().unwrap(),
             ));
+            covered.insert(*sensors.last().unwrap());
+            covered.insert(*beacons.last().unwrap());
         });
 
-    let row = 2000000;
-    for (i, sensor) in sensors.iter().enumerate() {
-        let dist_beacon = dist(*sensor, beacons[i]);
-        let dist_row = dist(*sensor, (sensor.0, row));
-        if dist_row <= dist_beacon {
-            for x in sensor.0 - dist_beacon..=sensor.0 + dist_beacon {
-                if dist((x, row), *sensor) <= dist_beacon
-                    && !sensors.contains(&(x, row))
-                    && !beacons.contains(&(x, row))
-                {
-                    covered.insert((x, row));
-                }
+    let y_range = 0..=4000000;
+    let mut covered_ranges: Vec<Vec<(isize, isize)>> = vec![];
+    for row in y_range {
+        let mut ranges: Vec<(isize, isize)> = vec![];
+        for (i, sensor) in sensors.iter().enumerate() {
+            let dist_beacon = dist(*sensor, beacons[i]);
+            let dist_row = dist(*sensor, (sensor.0, row));
+            if dist_row <= dist_beacon {
+                ranges.push((
+                    sensor.0 - dist_beacon + dist_row,
+                    sensor.0 + dist_beacon - dist_row,
+                ));
             }
         }
+        ranges.sort_by(|a, b| a.0.cmp(&b.0));
+
+        let mut merged_ranges: Vec<(isize, isize)> = vec![ranges[0]];
+        for i in 1..ranges.len() {
+            if ranges[i].0 <= merged_ranges.last().unwrap().1 {
+                merged_ranges.last_mut().unwrap().1 =
+                    merged_ranges.last().unwrap().1.max(ranges[i].1);
+            } else {
+                merged_ranges.push(ranges[i]);
+            }
+        }
+        covered_ranges.push(merged_ranges);
     }
 
-    println!("Day 15 - Part One: {}", covered.len());
+    let row = 2000000;
+    let covered_in_row = covered.iter().filter(|(_, y)| *y == row).count() as isize;
+    let result_one = covered_ranges[row as usize]
+        .iter()
+        .map(|(a, b)| b - a + 1)
+        .sum::<isize>()
+        - covered_in_row;
+
+    let mut distress_beacon: (isize, isize) = (0, 0);
+    for (i, ranges) in covered_ranges.iter().enumerate() {
+        if ranges.len() > 1 {
+            distress_beacon.0 = (ranges[1].0 + ranges[0].1) / 2;
+            distress_beacon.1 = i as isize;
+            break;
+        }
+    }
+    let result_two = distress_beacon.0 * 4000000 + distress_beacon.1;
+
+    println!("Day 15 - Part One: {}", result_one);
+    println!("Day 15 - Part Two: {}", result_two);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,6 @@
-mod day01;
-mod day02;
-mod day03;
-mod day04;
-mod day05;
-mod day06;
-mod day07;
-mod day08;
-mod day09;
-mod day10;
-mod day11;
-mod day12;
-mod day13;
-mod day14;
 mod day15;
-
-use crate::day01::solve as day01;
-use crate::day02::solve as day02;
-use crate::day03::solve as day03;
-use crate::day04::solve as day04;
-use crate::day05::solve as day05;
-use crate::day06::solve as day06;
-use crate::day07::solve as day07;
-use crate::day08::solve as day08;
-use crate::day09::solve as day09;
-use crate::day10::solve as day10;
-use crate::day11::solve as day11;
-use crate::day12::solve as day12;
-use crate::day13::solve as day13;
-use crate::day14::solve as day14;
 use crate::day15::solve as day15;
 
 fn main() {
-    day01();
-    day02();
-    day03();
-    day04();
-    day05();
-    day06();
-    day07();
-    day08();
-    day09();
-    day10();
-    day11();
-    day12();
-    day13();
-    day14();
     day15();
 }
