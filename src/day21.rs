4ab90bd1f508cd1b39904ab5d3212b93c303e71c
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-21 09:02:21 +0100

    Solve Day 21 (part one)

--- src/day21.rs
+++ src/day21.rs
@@ -24,39 +24,40 @@ pub fn solve() {
     println!("{:?}", operators);
 
     while !numbers.contains_key("root") {
-        let mut queue: Vec<String> = vec!["root".to_string()];
-        let mut visited: Vec<String> = vec![];
-        while queue.len() > 0 {
-            let current = queue.pop().unwrap();
-            visited.push(current.clone());
-            println!("Visiting {}", current);
-            if neighbors.contains_key(&current) {
-                let neighbor = neighbors.get(&current).unwrap();
-                let mut calculate = true;
-                neighbor.iter().for_each(|n| {
-                    if !numbers.contains_key(n) {
-                        calculate = false;
-                        if !visited.contains(n) {
-                            queue.push(n.to_string());
-                            println!("Pushing {}", n);
-                        }
-                    }
-                });
-                if calculate {
-                    let a = *numbers.get(&neighbor[0]).unwrap();
-                    let b = *numbers.get(&neighbor[1]).unwrap();
-                    let op = operators.get(&current).unwrap();
-                    let result = match op.as_str() {
-                        "+" => a + b,
-                        "-" => a - b,
-                        "*" => a * b,
-                        "/" => a / b,
-                        _ => panic!("Unknown operator"),
-                    };
-                    numbers.insert(current.clone(), result);
-                    println!("{} = {} {} {} = {}", current, a, op, b, result);
-                }
+        dfs("root".to_string(), &neighbors, &mut numbers, &operators);
+    }
+
+    println!("Day 21 - Part One: {}", numbers.get("root").unwrap());
+}
+
+fn dfs(
+    current: String,
+    neighbors: &HashMap<String, Vec<String>>,
+    numbers: &mut HashMap<String, isize>,
+    operators: &HashMap<String, String>,
+) {
+    if neighbors.contains_key(&current) {
+        let neighbor = neighbors.get(&current).unwrap();
+        let mut calculate = true;
+        neighbor.iter().for_each(|n| {
+            if !numbers.contains_key(n) {
+                calculate = false;
+                dfs(n.to_string(), neighbors, numbers, operators);
             }
+        });
+        if calculate {
+            let a = *numbers.get(&neighbor[0]).unwrap();
+            let b = *numbers.get(&neighbor[1]).unwrap();
+            let op = operators.get(&current).unwrap();
+            let result = match op.as_str() {
+                "+" => a + b,
+                "-" => a - b,
+                "*" => a * b,
+                "/" => a / b,
+                _ => panic!("Unknown operator"),
+            };
+            numbers.insert(current.clone(), result);
+            println!("{} = {} {} {} = {}", current, a, op, b, result);
         }
     }
 }
