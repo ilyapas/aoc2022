39da113953e4c9b92d1b83a33bf3d58769adc37e
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-21 11:48:33 +0100

    Solve Day 21

--- src/day21.rs
+++ src/day21.rs
@@ -5,6 +5,7 @@ pub fn solve() {
     let mut neighbors: HashMap<String, Vec<String>> = HashMap::new();
     let mut numbers: HashMap<String, isize> = HashMap::new();
     let mut operators: HashMap<String, String> = HashMap::new();
+    let mut humn_path: Vec<String> = vec![];
     input.lines().for_each(|line| {
         let line = line.replace(":", "");
         match line.split_whitespace().collect::<Vec<&str>>().as_slice() {
@@ -19,15 +20,62 @@ pub fn solve() {
             _ => (),
         }
     });
-    println!("{:?}", neighbors);
-    println!("{:?}", numbers);
-    println!("{:?}", operators);
 
     while !numbers.contains_key("root") {
-        dfs("root".to_string(), &neighbors, &mut numbers, &operators);
+        dfs(
+            "root".to_string(),
+            &neighbors,
+            &mut numbers,
+            &operators,
+            &mut humn_path,
+        );
+    }
+
+    let root_neighbors = neighbors.get("root").unwrap();
+    let root_left = root_neighbors[0].clone();
+    let root_right = root_neighbors[1].clone();
+    let mut oppisite = root_left.clone();
+    if humn_path.contains(&root_left) {
+        oppisite = root_right.clone();
+    }
+
+    // 150 = cczh / lfqf = x / 4 -> cczh = 150 * 4 = 600
+    // 600 = sllz + lgvd = 4 + x -> lgvd = 600 - 4 = 596
+    // 596 = ljgn * ptdq = 2 * x -> ptdq = 596 / 2 = 298
+    // 298 = humn - dvpt = x - 3 -> humn = 298 + 3 = 301
+
+    let mut res = numbers[&oppisite];
+    for i in 1..humn_path.len() - 1 {
+        let current = humn_path[i].clone();
+        let neighbors = neighbors.get(&current).unwrap();
+        let mut other = (0, "".to_string());
+        for (i, n) in neighbors.iter().enumerate() {
+            if !humn_path.contains(n) {
+                other = (i, n.clone());
+            }
+        }
+        let operator = operators.get(&current).unwrap();
+        if operator == "-" {
+            if other.0 == 0 {
+                res = numbers[&other.1] - res;
+            } else {
+                res = res + numbers[&other.1];
+            }
+        } else if operator == "/" {
+            if other.0 == 0 {
+                res = numbers[&other.1] / res;
+            } else {
+                res = res * numbers[&other.1];
+            }
+        } else if operator == "+" {
+            res = res - numbers[&other.1];
+        } else if operator == "*" {
+            res = res / numbers[&other.1];
+        }
     }
 
     println!("Day 21 - Part One: {}", numbers.get("root").unwrap());
+    println!("Day 21 - Part Two: {}", res);
 }
 
 fn dfs(
@@ -35,14 +83,21 @@ fn dfs(
     neighbors: &HashMap<String, Vec<String>>,
     numbers: &mut HashMap<String, isize>,
     operators: &HashMap<String, String>,
+    humn_path: &mut Vec<String>,
 ) {
+    if humn_path.len() == 0 || humn_path.last().unwrap() != "humn" {
+        humn_path.push(current.clone());
+    }
     if neighbors.contains_key(&current) {
         let neighbor = neighbors.get(&current).unwrap();
         let mut calculate = true;
         neighbor.iter().for_each(|n| {
             if !numbers.contains_key(n) {
                 calculate = false;
-                dfs(n.to_string(), neighbors, numbers, operators);
+                dfs(n.to_string(), neighbors, numbers, operators, humn_path);
+            }
+            if n == "humn" && humn_path.last().unwrap() != "humn" {
+                humn_path.push(n.to_string());
             }
         });
         if calculate {
@@ -57,7 +112,9 @@ fn dfs(
                 _ => panic!("Unknown operator"),
             };
             numbers.insert(current.clone(), result);
-            println!("{} = {} {} {} = {}", current, a, op, b, result);
         }
+    }
+    if humn_path.len() > 0 && humn_path.last().unwrap() != "humn" {
+        humn_path.pop();
     }
 }
