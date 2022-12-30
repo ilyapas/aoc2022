47d966b1c212554a44ed6df3a988a82812ec3bf4
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2022-12-30 23:54:01 +0100

    Solve Day 19

--- src/day19.rs
+++ src/day19.rs
@@ -1,18 +1,10 @@
-use std::collections::{HashSet, VecDeque};
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
-struct Quantity {
-    ore: usize,
-    clay: usize,
-    obsidian: usize,
-    geode: usize,
-}
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct State {
-    time: usize,
-    materials: Quantity,
-    robots: Quantity,
+    time: isize,
+    materials: [usize; 4],
+    robots: [usize; 4],
 }
 
 pub fn solve() {
@@ -30,133 +22,121 @@ pub fn solve() {
 
     let init_state = State {
         time: 0,
-        materials: Quantity {
-            ore: 0,
-            clay: 0,
-            obsidian: 0,
-            geode: 0,
-        },
-        robots: Quantity {
-            ore: 1,
-            clay: 0,
-            obsidian: 0,
-            geode: 0,
-        },
+        materials: [0, 0, 0, 0],
+        robots: [1, 0, 0, 0],
     };
 
-    let mut result = 0;
-    for (i, blueprint) in blueprints.iter().enumerate() {
-        println!("{:?}", blueprint);
-        let req_ore = Quantity {
-            ore: blueprint[0],
-            ..Default::default()
-        };
-        let req_clay = Quantity {
-            ore: blueprint[1],
-            ..Default::default()
-        };
-        let req_obsidian = Quantity {
-            ore: blueprint[2],
-            clay: blueprint[3],
-            ..Default::default()
-        };
-        let req_geode = Quantity {
-            ore: blueprint[4],
-            obsidian: blueprint[5],
-            ..Default::default()
-        };
-
-        let mut queue: VecDeque<State> = VecDeque::new();
-        let mut visited: HashSet<State> = HashSet::new();
-        queue.push_back(init_state.clone());
-
-        let mut counter = 0;
-        while queue.len() > 0 {
-            let current = queue.pop_front().unwrap();
-            visited.insert(current.clone());
-
-            counter += 1;
-            if counter % 10000000 == 0 {
-                println!("{:?}", current);
+    let mut result = [0, 1];
+    let time_limit = [24, 32];
+    for part in 0..result.len() {
+        for (i, blueprint) in blueprints.iter().enumerate() {
+            if part == 1 && i > 2 {
+                break;
             }
 
-            let mut next = current.clone();
+            let req: [[usize; 3]; 4] = [
+                [blueprint[0], 0, 0],
+                [blueprint[1], 0, 0],
+                [blueprint[2], blueprint[3], 0],
+                [blueprint[4], 0, blueprint[5]],
+            ];
 
-            next.time += 1;
-            if next.time > 24 {
-                continue;
+            let max_req: [usize; 3] = [
+                req.iter().map(|r| r[0]).max().unwrap(),
+                req.iter().map(|r| r[1]).max().unwrap(),
+                req.iter().map(|r| r[2]).max().unwrap(),
+            ];
+
+            let mut cache: HashMap<State, usize> = HashMap::new();
+            let max_geode = dfs(
+                State {
+                    time: time_limit[part],
+                    ..init_state.clone()
+                },
+                &mut cache,
+                req,
+                max_req,
+            );
+
+            if part == 0 {
+                result[part] += (i + 1) * max_geode;
+            } else {
+                result[part] *= max_geode;
             }
+        }
+    }
 
-            next.materials.ore += current.robots.ore;
-            next.materials.clay += current.robots.clay;
-            next.materials.obsidian += current.robots.obsidian;
-            next.materials.geode += current.robots.geode;
+    println!("Day 19 - Part One: {}", result[0]);
+    println!("Day 19 - Part Two: {}", result[1]);
+}
 
-            if !visited.contains(&next) {
-                queue.push_back(next.clone());
-            }
+fn dfs(
+    state: State,
+    cache: &mut HashMap<State, usize>,
+    req: [[usize; 3]; 4],
+    max_req: [usize; 3],
+) -> usize {
+    if state.time == 0 {
+        return state.materials[3];
+    }
+    if cache.contains_key(&state) {
+        return *cache.get(&state).unwrap();
+    }
 
-            if current.materials.ore >= req_geode.ore
-                && current.materials.obsidian >= req_geode.obsidian
-            {
-                let mut n = next.clone();
-                n.materials.ore -= req_geode.ore;
-                n.materials.obsidian -= req_geode.obsidian;
-                n.robots.geode += 1;
-                if !visited.contains(&n) {
-                    queue.push_back(n.clone());
+    let mut max_geode = state.materials[3] + state.robots[3] * state.time as usize;
+
+    for (i, recipe) in req.iter().enumerate() {
+        // if we produce more than we can spend in a round, skip
+        if i != 3 && state.robots[i] >= max_req[i] {
+            continue;
+        }
+
+        // Determine how much time we need to wait until we have enough materials for a new robot
+        let mut wait_time: isize = 0;
+        let mut can_build = true;
+        for (j, mat_req) in recipe.iter().enumerate() {
+            if *mat_req > 0 {
+                if state.robots[j] == 0 {
+                    can_build = false;
+                    break;
                 }
-                continue;
-            }
-
-            if current.materials.ore >= req_obsidian.ore
-                && current.materials.clay >= req_obsidian.clay
-                && current.robots.obsidian < req_geode.obsidian
-            {
-                let mut n = next.clone();
-                n.materials.ore -= req_obsidian.ore;
-                n.materials.clay -= req_obsidian.clay;
-                n.robots.obsidian += 1;
-                if !visited.contains(&n) {
-                    queue.push_back(n.clone());
-                }
-                continue;
-            }
-
-            if current.materials.ore >= req_clay.ore && current.robots.clay < req_obsidian.clay {
-                let mut n = next.clone();
-                n.materials.ore -= req_clay.ore;
-                n.robots.clay += 1;
-                if !visited.contains(&n) {
-                    queue.push_back(n.clone());
-                }
-            }
-
-            if current.materials.ore >= req_ore.ore
-                && current.robots.ore
-                    < *[req_clay.ore, req_obsidian.ore, req_geode.ore]
-                        .iter()
-                        .max()
-                        .unwrap()
-            {
-                let mut n = next.clone();
-                n.materials.ore -= req_ore.ore;
-                n.robots.ore += 1;
-                if !visited.contains(&n) {
-                    queue.push_back(n.clone());
-                }
+                wait_time = std::cmp::max(
+                    wait_time,
+                    ((*mat_req as f64 - state.materials[j] as f64) / state.robots[j] as f64).ceil()
+                        as isize,
+                );
             }
         }
 
-        result += (i + 1)
-            * visited
-                .iter()
-                .filter(|s| s.time == 24)
-                .max_by_key(|s| s.materials.geode)
-                .unwrap()
-                .materials
-                .geode;
+        if !can_build {
+            continue;
+        }
+
+        let mut next = state.clone();
+        next.time = state.time - wait_time as isize - 1;
+        if next.time <= 0 {
+            continue;
+        }
+
+        // Accumulate the materials we have produced during wait time
+        for j in 0..4 {
+            next.materials[j] += next.robots[j] * (wait_time as usize + 1);
+        }
+
+        // Subtract the materials we need for the new robot
+        for (j, mat_req) in recipe.iter().enumerate() {
+            next.materials[j] -= mat_req;
+        }
+
+        // Throw away the excess materials
+        for j in 0..3 {
+            next.materials[j] = std::cmp::min(next.materials[j], max_req[j] * next.time as usize);
+        }
+
+        next.robots[i] += 1;
+        max_geode = std::cmp::max(max_geode, dfs(next, cache, req, max_req));
     }
 
-    println!("Day 19 - Part One: {}", result);
+    cache.insert(state, max_geode);
+    max_geode
 }
